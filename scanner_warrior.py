c693c8d508270ef6be48ddc201bc50945d3fecd5
bionicfrog-inc <paul.boisclair@gmail.com>
2026-06-05 17:51:43 -0400

    Update scanner_warrior.py

--- scanner_warrior.py
+++ scanner_warrior.py
@@ -48,6 +48,10 @@ print("MIN_RVOL =", MIN_RVOL)
 # Charger les symboles
 # =====================================================
 
+# =====================================================
+# Charger les symboles
+# =====================================================
+
 try:
     with open("watchlist.txt", "r") as f:
         symbols = [
@@ -56,6 +60,15 @@ try:
             if line.strip() and not line.startswith("#")
         ]
 
+    print(f"NB SYMBOLS = {len(symbols)}")
+
+    symbols = symbols[:5]
+
+    print("MODE TEST : seulement 5 symboles")
+
+except Exception as e:
+    print(f"❌ watchlist.txt introuvable : {e}")
+    exit()
     
 
 results = []
